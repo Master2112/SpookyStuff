c0e84baed6e5f5b286dd49b1e495b9a21f34958a
Tim Falken <timfalken@hotmail.com>
2015-10-09 10:17:42 +0200

    Scenario

---
+++ SpookyStuff/SpookyStuff/Models/Scenario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpookyStuff.Models
+{
+    public class Scenario
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        public int SuppliesId { get; set; }
+
+        public virtual Supplies SuppliesNeeded
+        {
+            get
+            {
+                return suppliesNeeded;
+            }
+
+            set
+            {
+                SuppliesId = value.Id;
+                suppliesNeeded = value;
+            }
+        }
+
+        private Supplies suppliesNeeded { get; set; }
+    }
+}
