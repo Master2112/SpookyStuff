16704dfd353dc2d130264b9e2a6ef211ead71ca5
xxAmberrrr <amber__12@hotmail.com>
2015-10-09 10:04:16 +0200

    De class supplies

---
+++ SpookyStuff/SpookyStuff/Models/Supplies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpookyStuff.Models
+{
+    public class Supplies
+    {
+        public int Id { get; set; }
+        public TraumaTypes TraumaTypes { get; set; }
+        public string Name { get; set; }
+        public string Beschrijving { get; set; }
+    }
+}
