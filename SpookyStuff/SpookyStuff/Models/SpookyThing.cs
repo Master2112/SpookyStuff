31a6721156d777d88a47cb3f81456efa77ce9f95
Tim Falken <timfalken@hotmail.com>
2015-10-09 10:00:52 +0200

    Enum & item class

--- SpookyStuff/SpookyStuff/Models/SpookyThing.cs
+++ SpookyStuff/SpookyStuff/Models/SpookyThing.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpookyStuff.Models
 {
-    public class SpookyThing
+    public class SpookyDecoration
     {
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        public string Description { get; set; }
+        public TraumaTypes Type { get; set; }
+        public int TraumaIntensity { get; set; }
     }
 }
---
+++ SpookyStuff/SpookyStuff/Models/TraumaTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpookyStuff.Models
+{
+    public enum TraumaTypes
+    {
+        Meh,
+        Spooky,
+        Ded
+    }
+}
